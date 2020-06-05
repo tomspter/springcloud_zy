e0fec7b37726ba1b41c83d90579f1b9c385a0e3c
tomspter <2501760110@qq.com>
2020-06-05 18:53:06 +0800

    eureka

--- cloud-eureka-server7003/src/main/java/com/tomspter/springcloud/EurekaMain7003.java
+++ cloud-eureka-server7003/src/main/java/com/tomspter/springcloud/EurekaMain7003.java
@@ -2,12 +2,12 @@ package com.tomspter.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderApplication80 {
+@EnableEurekaServer
+public class EurekaMain7003 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderApplication80.class, args);
+        SpringApplication.run(EurekaMain7003.class,args);
     }
 }
