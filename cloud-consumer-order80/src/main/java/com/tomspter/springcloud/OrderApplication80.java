b9bfeacfc802151ee40d35b6cb9e97812aa1f279
tomspter <2501760110@qq.com>
2020-06-05 17:40:41 +0800

    微服务架构构建完成

---
+++ cloud-consumer-order80/src/main/java/com/tomspter/springcloud/OrderApplication80.java
@@ -0,0 +1,11 @@
+package com.tomspter.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication80.class, args);
+    }
+}
